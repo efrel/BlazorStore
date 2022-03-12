f6932f62c3719bb38a1370b7d1c74f517c996545
Efrel Armando López Cáceres <programmer@ferrebaratillohn.com>
2022-03-12 16:05:34 -0600

    Se construyo de módulo de noticias.

---
+++ Shared/News.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Shared
+{
+    public class News
+    {
+        public Guid NewsId {get;set;}
+        public string Description {get; set;}
+        public string Image {get;set;}
+        public string Url {get;set;}
+    }
+}
